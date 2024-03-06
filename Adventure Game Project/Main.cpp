6d720c9432a05d8c1382ed79c8c10481f8804528
Sarah Herzog <77997296+SarahHerzog@users.noreply.github.com>
2024-03-06 12:18:42 +0000

    Loops added

--- Adventure Game Project/Main.cpp
+++ Adventure Game Project/Main.cpp
@@ -17,12 +17,13 @@ int main()
 	vector <string> roomDescs = { 
 		"A barren, weed-filled courtyard of a crumbling ruined castle.", 
 		"A once-grand hll with a rotting red carpet, lined with rusted suits of armour..",
-		"The dilapidated gates hang crookedly from their hinges, set into the crumbling stone wall that surrounds the ruined castle." };
-
-	int playerStamina = 100;
-	string playerChoice = "";
-	int newRoomIndex = -1;
+		"The dilapidated gates hang crookedly from their hinges, set into the crumbling stone wall that surrounds the ruined castle." 
+	};
+	int playerChoice = -1;
 	int moveCost = 5;
+	int playerStamina = 20;
+	int roomIndex = 0;
+	bool exit = false;
 
 	// DISPLAY the title of the program.
 	cout << "Welcome to the Example Adventure Game!" << endl;
@@ -37,55 +38,78 @@ int main()
 	// DISPLAY a greeting to the player, using the playerName variable
 	cout << "Welcome, " << playerName << ", soon to be hero of the realm!" << endl << endl;
 
-	// DISPLAY the name of the room where the player currently is, using the room1Name variable
-	cout << "You find yourself in the " << roomNames[0] << " - ";
-
-	// DISPLAY the description of the room where the player currently is, using the room1Desc variable
-	cout << roomDescs[0] << endl << endl;
-
-	// DISPLAY the player's current stamina, using the playerStamina variable
-	cout << "Stamina remaining: " << playerStamina << endl;
-
-	// DISPLAY the names of the other two rooms, numbered, 
-	// as potential exits, and ask the player where they want to go
-	cout << "Exits are:" << endl;
-	cout << "1.   " << roomNames[1] << endl;
-	cout << "2.   " << roomNames[2] << endl;
-	cout << "Where would you like to go?" << endl;
-
-	// READ the player's response into the playerChoice variable
-	getline(cin, playerChoice);
-
-	// IF the player has chosen the first room...
-	if (playerChoice == "1")
+	// WHILE exit is false...
+	while (!exit)
 	{
-		// SET the index to 1
-		newRoomIndex = 1;
-	}
-	// ELSE IF the player has chosen the second room...
-	else if (playerChoice == "2")
-	{
-		// SET the index to 2
-		newRoomIndex = 2;
-	}
-	// ELSE 
-	else
-	{
-		// DISPLAY an error message due to unrecognised input
-		cout << "Sorry, I couldn't understand what you meant by \"" << playerChoice << "\"" << endl;
-	}
-	// ENDIF
+		// DISPLAY the name of the room where the player currently is, using the roomNames vector and roomIndex
+		cout << "You find yourself in the " << roomNames[roomIndex] << " - ";
 
-	// IF the new room index is not -1 (it is valid)
-	if (newRoomIndex != -1)
-	{
-		// SUBTRACT the stamina required to move from the player's stamina value
-		playerStamina = playerStamina - moveCost;
+		// DISPLAY the description of the room where the player currently is, using the roomDescs vector and roomIndex
+		cout << roomDescs[roomIndex] << endl << endl;
 
-		// DISPLAY the name and description of the chosen room and the player's
-		// new stamina
-		cout << "You move to the " << roomNames[newRoomIndex] << " - ";
-		cout << roomDescs[newRoomIndex] << endl << endl;
+		// DISPLAY the player's current stamina, using the playerStamina variable
 		cout << "Stamina remaining: " << playerStamina << endl;
+
+		// DISPLAY a message asking the player where they want to go
+		cout << "Where would you like to go? (type the number)" << endl;
+		cout << "Exits are:" << endl;
+
+		// FOR each room in the roomNames vector...
+		for (unsigned int i = 0; i < roomNames.size(); ++i)
+		{
+			// DISPLAY the loop index + 1 and the name of the room at that index
+			cout << (i+1) << ".   " << roomNames[i] << endl;
+		}
+		// END FOR
+
+		// DISPLAY a final option to quit, numbered equal to the size of roomNames+1
+		cout << (roomNames.size() + 1) << ".   " << "QUIT" << endl;
+
+
+		// READ the player's response into the playerChoice variable
+		cin >> playerChoice;
+
+		// IF the player's choice between 1 and the roomNames vector size...
+		if (playerChoice >= 1 && playerChoice <= roomNames.size())
+		{
+			// SET the new roomIndex to be the player's choice minus one
+			roomIndex = playerChoice - 1;
+
+			// SUBTRACT the stamina required to move from the player's stamina value
+			playerStamina = playerStamina - moveCost;
+
+			// DISPLAY a message that the player has moved and used moveCost stamina
+			cout << "You have changed rooms and used " << moveCost << " stamina." << endl;
+		}
+		// ELSE IF the player's choice is equal to the size of the room vector...
+		else if (playerChoice == roomNames.size()+1)
+		{
+			// DISPLAY a goodbye message to the player
+			cout << "Thank you for playing!" << endl;
+
+			// SET exit to true
+			exit = true;
+		}
+		// ELSE 
+		else
+		{
+			// DISPLAY an error message due to unrecognised input
+			cout << "Sorry, the number you typed was not one of the options: " << playerChoice << endl;
+		}
+		// END IF
+
+		// IF the player stamina reaches 0 or lower...
+		if (playerStamina <= 0)
+		{
+			// DISPLAY a message to the player telling them they have died
+			cout << "You have run out of stamina and died! Better luck next time. Please play again!" << endl;
+
+			// SET exit to true
+			exit = true;
+		}
+		// ENDIF
+
 	}
+	// END WHILE
+
 }
