ead7fe879df7c46a937bfe36573c73f3f226af6b
Sarah Herzog <77997296+SarahHerzog@users.noreply.github.com>
2024-03-06 10:33:01 +0000

    Vectors!

--- Adventure Game Project/Main.cpp
+++ Adventure Game Project/Main.cpp
@@ -2,6 +2,7 @@
 // Libraries
 #include <string>
 #include <iostream>
+#include <vector>
 
 // Needed to avoid having to type std:: a lot!
 using namespace std;
@@ -12,9 +13,16 @@ int main()
 
 	// INITIALISE variables
 	string playerName = "";
-	string room1Name = "Courtyard";
-	string room1Desc = "A barren, weed-filled courtyard of a crumbling ruined castle.";
+	vector <string> roomNames = { "Courtyard", "Entry Hall", "Gate" };
+	vector <string> roomDescs = { 
+		"A barren, weed-filled courtyard of a crumbling ruined castle.", 
+		"A once-grand hll with a rotting red carpet, lined with rusted suits of armour..",
+		"The dilapidated gates hang crookedly from their hinges, set into the crumbling stone wall that surrounds the ruined castle." };
+
 	int playerStamina = 100;
+	string playerChoice = "";
+	int newRoomIndex = -1;
+	int moveCost = 5;
 
 	// DISPLAY the title of the program.
 	cout << "Welcome to the Example Adventure Game!" << endl;
@@ -30,32 +38,19 @@ int main()
 	cout << "Welcome, " << playerName << ", soon to be hero of the realm!" << endl << endl;
 
 	// DISPLAY the name of the room where the player currently is, using the room1Name variable
-	cout << "You find yourself in the " << room1Name << " - ";
+	cout << "You find yourself in the " << roomNames[0] << " - ";
 
 	// DISPLAY the description of the room where the player currently is, using the room1Desc variable
-	cout << room1Desc << endl << endl;
+	cout << roomDescs[0] << endl << endl;
 
 	// DISPLAY the player's current stamina, using the playerStamina variable
 	cout << "Stamina remaining: " << playerStamina << endl;
 
-
-	//----------
-	// WEEK 3
-	//----------
-
-	// INITIALISE variablesa
-	string room2Name = "Entry Hall";
-	string room2Desc = "A once-grand hll with a rotting red carpet, lined with rusted suits of armour..";
-	string room3Name = "Gate";
-	string room3Desc = "The dilapidated gates hang crookedly from their hinges, set into the crumbling stone wall that surrounds the ruined castle.";
-	string playerChoice = "";
-	int moveCost = 5;
-
 	// DISPLAY the names of the other two rooms, numbered, 
 	// as potential exits, and ask the player where they want to go
 	cout << "Exits are:" << endl;
-	cout << "1.   " << room2Name << endl;
-	cout << "2.   " << room3Name << endl;
+	cout << "1.   " << roomNames[1] << endl;
+	cout << "2.   " << roomNames[2] << endl;
 	cout << "Where would you like to go?" << endl;
 
 	// READ the player's response into the playerChoice variable
@@ -64,26 +59,14 @@ int main()
 	// IF the player has chosen the first room...
 	if (playerChoice == "1")
 	{
-		// SUBTRACT the stamina required to move from the player's stamina value
-		playerStamina = playerStamina - moveCost;
-
-		// DISPLAY the name and description of the chosen room and the player's
-		// new stamina
-		cout << "You move to the " << room2Name << " - ";
-		cout << room2Desc << endl << endl;
-		cout << "Stamina remaining: " << playerStamina << endl;
+		// SET the index to 1
+		newRoomIndex = 1;
 	}
 	// ELSE IF the player has chosen the second room...
 	else if (playerChoice == "2")
 	{
-		// SUBTRACT the stamina required to move from the player's stamina value
-		playerStamina = playerStamina - moveCost;
-
-		// DISPLAY the name and description of the chosen room and the player's
-		// new stamina
-		cout << "You move to the " << room3Name << " - ";
-		cout << room3Desc << endl << endl;
-		cout << "Stamina remaining: " << playerStamina << endl;
+		// SET the index to 2
+		newRoomIndex = 2;
 	}
 	// ELSE 
 	else
@@ -93,5 +76,16 @@ int main()
 	}
 	// ENDIF
 
+	// IF the new room index is not -1 (it is valid)
+	if (newRoomIndex != -1)
+	{
+		// SUBTRACT the stamina required to move from the player's stamina value
+		playerStamina = playerStamina - moveCost;
 
+		// DISPLAY the name and description of the chosen room and the player's
+		// new stamina
+		cout << "You move to the " << roomNames[newRoomIndex] << " - ";
+		cout << roomDescs[newRoomIndex] << endl << endl;
+		cout << "Stamina remaining: " << playerStamina << endl;
+	}
 }
